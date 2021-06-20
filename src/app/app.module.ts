5def77e21e6f84b91d91009435ee0842294635fe
fatmaoz <fatmas199021@gmail.com>
2021-06-20 17:42:57 +0100

    Lazy Loading for Admin Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,7 @@ import { ShopComponent } from './shop/shop.component';
        { path: 'shop', component: ShopComponent },
        { path: 'cart', component: CartDetailComponent },
        { path: 'checkout', component: CheckoutComponent },
-       { path: 'admin', loadChildren : './admin/admin.module#AdminModule'},
+       { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m=>m.AdminModule) },
        { path: '**', redirectTo: "/shop" }
     ])
   ],
