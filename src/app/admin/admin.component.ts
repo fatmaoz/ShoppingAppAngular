630dc564b20e6a2170321eab91e9050d6c6e22b5
fatmaoz <fatmas199021@gmail.com>
2021-06-21 19:52:58 +0100

    Logout

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,9 +1,17 @@
+import { AuthService } from './../model/auth.service';
 import { Component } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl:'admin.component.html'
 })
 
 export class AdminComponent {
-  
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  logout(){
+    this.authService.clear();
+    this.router.navigateByUrl('/shop');
+  }
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,12 +6,24 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AuthComponent } from './auth/auth.component';
 import { FormsModule } from '@angular/forms';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductFormComponent } from './products/product-form/product-form.component';
+import { CategoryListComponent } from './categories/category-list/category-list.component';
+import { CategoryFormComponent } from './categories/category-form/category-form.component';
+import { OrderFormComponent } from './orders/order-form/order-form.component';
+import { OrderListComponent } from './orders/order-list/order-list.component';
 
 
 @NgModule({
   declarations: [
     AuthComponent,
-    AdminComponent
+    AdminComponent,
+    ProductListComponent,
+    ProductFormComponent,
+    CategoryListComponent,
+    CategoryFormComponent,
+    OrderFormComponent,
+    OrderListComponent
   ],
   imports: [
     CommonModule,
